08d1f2e1b888ab644ebc63eaf60b10b88912cf68
JaneErr <yanmalievskii@gmail.com>
2020-11-26 23:27:47 +1000

    Добавьте файлы проекта.

---
+++ bakery/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace bakery
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.tabPage6);
+            this.tabControl1.Controls.Add(this.tabPage7);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1444, 25);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.Selected += new System.Windows.Forms.TabControlEventHandler(this.tabControl1_Selected);
+            this.tabControl1.Deselected += new System.Windows.Forms.TabControlEventHandler(this.tabControl1_Deselected);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1436, 0);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Заказы";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1436, -8);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Контракты";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 29);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1436, -8);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Поставщики";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 29);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(1436, -8);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Заказчики";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 29);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(1436, -8);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Сотрудники";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Location = new System.Drawing.Point(4, 29);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage6.Size = new System.Drawing.Size(1436, -8);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "Продукция";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // tabPage7
+            // 
+            this.tabPage7.Location = new System.Drawing.Point(4, 29);
+            this.tabPage7.Name = "tabPage7";
+            this.tabPage7.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage7.Size = new System.Drawing.Size(1436, -8);
+            this.tabPage7.TabIndex = 6;
+            this.tabPage7.Text = "Материалы";
+            this.tabPage7.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1444, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(1444, 649);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            this.dataGridView1.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.dataGridView1_EditingControlShowing);
+            this.dataGridView1.UserAddedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView1_UserAddedRow);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1444, 673);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.TabPage tabPage7;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ bakery/Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace bakery
+{
+    public partial class Form1 : Form
+    {
+
+        private SqlConnection sqlConnection = null;
+
+        private SqlCommandBuilder sqlBuilder = null;
+
+        private SqlDataAdapter sqlDataAdapter = null;
+
+        private DataSet dataSet = null;
+
+        private string currentTable = "Orders";
+
+        private bool newRowAdding = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                sqlDataAdapter = new SqlDataAdapter("Select *, 'Delete' AS [Commands] FROM " + currentTable, sqlConnection);
+
+                sqlBuilder = new SqlCommandBuilder(sqlDataAdapter);
+
+                sqlBuilder.GetInsertCommand();
+                sqlBuilder.GetDeleteCommand();
+                sqlBuilder.GetUpdateCommand();
+
+                dataSet = new DataSet();
+
+                sqlDataAdapter.Fill(dataSet, currentTable);
+
+                dataGridView1.DataSource = dataSet.Tables[currentTable];
+
+                int c = dataGridView1.ColumnCount - 1;
+
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+                    dataGridView1[c, i] = linkCell;
+                }
+
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ReloadData()
+        {
+            try
+            {
+                dataSet.Tables[currentTable].Clear();
+
+                sqlDataAdapter.Fill(dataSet, currentTable);
+
+                dataGridView1.DataSource = dataSet.Tables[currentTable];
+
+                int c = dataGridView1.ColumnCount - 1;
+
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+                    dataGridView1[c, i] = linkCell;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var projectPath = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory())));
+            AppDomain.CurrentDomain.SetData("DataDirectory", projectPath);
+            SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+            stringBuilder["Data Source"] = "(LocalDB)\\MSSQLLocalDB";
+            stringBuilder["AttachDbFilename"] = projectPath + "\\data\\bakerydb.mdf";
+            stringBuilder["Integrated Security"] = true;
+            sqlConnection = new SqlConnection(stringBuilder.ConnectionString);
+            sqlConnection.Open();
+            LoadData();
+        }
+
+        private void tabControl1_Selected(object sender, TabControlEventArgs e)
+        {
+            if (e.TabPage.Name == "tabPage1") 
+            {
+                currentTable = "Orders";
+            }
+            else if (e.TabPage.Name == "tabPage2")
+            {
+                currentTable = "Contracts";
+            }
+            else if (e.TabPage.Name == "tabPage3")
+            {
+                currentTable = "Providers";
+            }
+            else if (e.TabPage.Name == "tabPage4")
+            {
+                currentTable = "Customers";
+            }
+            else if (e.TabPage.Name == "tabPage5")
+            {
+                currentTable = "Employees";
+            }
+            else if (e.TabPage.Name == "tabPage6")
+            {
+                currentTable = "Production";
+            }
+            else if (e.TabPage.Name == "tabPage7")
+            {
+                currentTable = "Materials";
+            }
+            LoadData();
+        }
+
+        private void tabControl1_Deselected(object sender, TabControlEventArgs e)
+        {
+            dataGridView1.DataSource = null;
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (e.ColumnIndex == dataGridView1.ColumnCount - 1)
+                {
+                    string task = dataGridView1.Rows[e.RowIndex].Cells[dataGridView1.ColumnCount - 1].Value.ToString();
+
+                    if (task == "Delete")
+                    {
+                        if (MessageBox.Show("Удалить эту строку?", "Удаление", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            dataGridView1.Rows.RemoveAt(e.RowIndex);
+
+                            dataSet.Tables[currentTable].Rows[e.RowIndex].Delete();
+
+                            sqlDataAdapter.Update(dataSet, currentTable);
+                        }
+                    }
+                    else if (task == "Insert")
+                    {
+                        int rowIndex = dataGridView1.RowCount - 2;
+
+                        DataRow row = dataSet.Tables[currentTable].NewRow();
+
+                        for (int i = 0; i < dataGridView1.ColumnCount - 1; i++)
+                        {
+                            var columnName = dataGridView1.Columns[i].Name;
+
+                            row[columnName] = dataGridView1.Rows[rowIndex].Cells[columnName].Value;
+                        }
+
+                        dataSet.Tables[currentTable].Rows.Add(row);
+
+                        dataSet.Tables[currentTable].Rows.RemoveAt(dataSet.Tables[currentTable].Rows.Count - 1);
+
+                        dataGridView1.Rows.RemoveAt(dataGridView1.Rows.Count - 2);
+
+                        dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = "Delete";
+
+                        sqlDataAdapter.Update(dataSet, currentTable);
+
+                        newRowAdding = false;
+                    }
+                    else if (task == "Update")
+                    {
+                        for (int i = 0; i < dataGridView1.ColumnCount - 1; i++)
+                        {
+                            var columnName = dataGridView1.Columns[i].Name;
+
+                            dataSet.Tables[currentTable].Rows[e.RowIndex][columnName] = dataGridView1.Rows[e.RowIndex].Cells[columnName].Value;
+                        }
+
+                        sqlDataAdapter.Update(dataSet, currentTable);
+
+                        dataGridView1.Rows[e.RowIndex].Cells[dataGridView1.ColumnCount - 1].Value = "Delete";
+                    }
+
+                    ReloadData();
+                }
+
+            } 
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView1_UserAddedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            try
+            {
+                if (newRowAdding == false)
+                {
+                    newRowAdding = true;
+
+                    int lastRow = dataGridView1.RowCount - 2;
+
+                    int lastColumn = dataGridView1.ColumnCount - 1;
+
+                    DataGridViewRow row = dataGridView1.Rows[lastRow];
+
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+
+                    dataGridView1[lastColumn, lastRow] = linkCell;
+
+                    row.Cells["Commands"].Value = "Insert";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (newRowAdding == false)
+                {
+                    int rowIndex = dataGridView1.SelectedCells[0].RowIndex;
+
+                    DataGridViewRow editingRow = dataGridView1.Rows[rowIndex];
+
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+
+                    dataGridView1[dataGridView1.ColumnCount - 1, rowIndex] = linkCell;
+
+                    editingRow.Cells["Commands"].Value = "Update";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView1_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            e.Control.KeyPress -= new KeyPressEventHandler(Int_Column_KeyPress);
+
+            e.Control.KeyPress -= new KeyPressEventHandler(Double_Column_KeyPress);
+
+            e.Control.KeyPress -= new KeyPressEventHandler(DateTime_Column_KeyPress);
+
+            object cellValue = dataGridView1.Rows[0].Cells[dataGridView1.CurrentCell.ColumnIndex].Value;
+
+            TextBox textBox = e.Control as TextBox;
+
+            if (cellValue is int || cellValue is decimal)
+            {
+                textBox.KeyPress += new KeyPressEventHandler(Int_Column_KeyPress);
+            }
+            else if (cellValue is DateTime)
+            {
+                textBox.KeyPress += new KeyPressEventHandler(DateTime_Column_KeyPress);
+            }
+            else if (cellValue is double)
+            {
+                textBox.KeyPress += new KeyPressEventHandler(Double_Column_KeyPress);
+            }    
+        }
+
+        private void Int_Column_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }    
+
+        }
+
+        private void Double_Column_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsNumber(e.KeyChar) && e.KeyChar != ',' && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void DateTime_Column_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsNumber(e.KeyChar) && e.KeyChar != '.' && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
